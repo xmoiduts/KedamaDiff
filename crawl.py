67522a9473503025586644a2466b2cc011c4fa2a
xmoiduts <xmoiduts@users.noreply.github.com>
2017-11-14 22:46:13 +0800

    New Function:Create today's image dir if not exist.

--- crawl.py
+++ crawl.py
@@ -1,9 +1,11 @@
-import requests,time
+import requests,os
+import time,datetime
+
 # https://map.nyaacat.com/kedama/v2_daytime/0/3/3/2/3/2/3/1/1.jpg?c=1510454854  
 map_domain='https://map.nyaacat.com/kedama'
 map_name='v2_daytime'
 refcode='c=1510454854'
-download_path=r'../KedamaDiff_Imgs/'
+download_path=r'images/'
 threads=10
 
 '''输入图块链接，返回              错误处理应该改进吧     '''
@@ -33,7 +35,17 @@ def makePicName(prefixes,depth):
             yield(['/'+'/'.join(prefix+quaternary)+'.jpg','_'+'_'.join(prefix+quaternary)+'.jpg'])
     return
 
-for i in makePicName(['/0/3/3/2','/0/3/3/3','/1/2/2/2','/1/2/2/3','/2/1/1/0','/2/1/1/1','/3/0/0/0','/3/0/0/1'],8):
+'''新建今日存图目录并以此替换download_path'''
+def createTodayDir(path):
+    today=datetime.datetime.today().strftime('%Y%m%d')
+    new_path=path+'/'+today+'/'
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    return new_path
+
+
+download_path = createTodayDir(download_path)
+for i in makePicName(['/1/2/2/2','/0/3/3/3','/0/3/3/2','/1/2/2/3','/2/1/1/0','/2/1/1/1','/3/0/0/0','/3/0/0/1'],8):
     picurl=map_domain+'/'+map_name+i[0]+'?'+refcode
     try:
         rh=teaseImage(picurl)
