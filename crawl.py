6656541fd75c9feaecc894f6510ada0d6b1ad6f1
xmoiduts <xmoiduts@users.noreply.github.com>
2017-11-25 12:36:36 +0800

    Use ETag to judge cache update status.
    Image history file sctucure changed

--- crawl.py
+++ crawl.py
@@ -15,14 +15,14 @@ crawl_zones=['/1/2/2/2','/0/3/3/3','/0/3/3/2','/1/2/2/3','/2/1/1/0','/2/1/1/1','
 crawl_level=8
 
 '''输入图块链接，返回              错误处理应该改进吧     '''
-def teaseImage(imageURL):
-    r=requests.head(imageURL,timeout=5) #只取响应头而不下载响应体
+def teaseImage(URL):
+    r=requests.head(URL,timeout=5) #只取响应头而不下载响应体
     return r.headers
     #print(r.headers['Last-Modified'],r.headers['Content-Length']) #404-"KeyError"error.
 
 '''下载给定URL的文件并重命名保存到指定目录中。'''
-def downloadImage(imageURL,path,filename):
-    r=requests.get(imageURL,stream='True')
+def downloadImage(URL,path,filename):
+    r=requests.get(URL,stream='True')
     if r.status_code==200:
         img=r.raw.read()
         with open(path+filename,'wb') as f:
@@ -42,14 +42,17 @@ def makePicName(prefixes,depth):
     return
 
 '''抓图线程'''
-def dealWithPicurl(pic_tuple,save_dir,download=False):
+def dealWithPicurl(pic_tuple,save_to,download=False):
     url=map_domain+'/'+map_name+pic_tuple[0]+'?'+refcode
     filename=pic_tuple[1]
     try:
-        rh=teaseImage(url)
+        
         if download==True:
-            downloadImage(url,save_dir,filename)
-        return({'Filename':filename,'Lastmod':rh['Last-Modified'],'Length':rh['Content-Length']})
+            downloadImage(url,save_to,filename)
+            return {'Filename':filename}
+        else:
+            rh=teaseImage(url)
+            return({'Path':pic_tuple,'Filename':filename,'Lastmod':rh['Last-Modified'],'ETag':rh['ETag']})
     except KeyError:
         return({'Filename':filename,'ERROR':'404'})
     except requests.exceptions.ConnectionError:
@@ -78,6 +81,8 @@ def runsDaily():
     figure_404=figure_Fail=Figure_ignore=Figure_added=Figure_update=0
     download=False      #日更不下载图片，只收录图片头长度发生了变化的图块信息
     log_buffer={}
+    download_queue=[]
+
 
     try:        #读取图块更新史，若文件不存在则空文件将被创建
         with open(data_folder+'/'+'update_history.txt','r') as f:
@@ -89,8 +94,9 @@ def runsDaily():
             f.write('{}')#先写个空dict防呆
 
     to_crawl=makePicName(crawl_zones,crawl_level)   #一个生成器
+    save_to=getImgdir(image_folder,download)
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:    #抓图工人线程池
-        for pic_info in executor.map(dealWithPicurl,to_crawl,getImgdir(image_folder,download),whetherDownload(download)): 
+        for pic_info in executor.map(dealWithPicurl,to_crawl,save_to,whetherDownload(download)): 
             if 'ERROR' in pic_info.keys():                                              #抓取图片头失败,pass
                 print(pic_info['ERROR']+'\t\t\t'+pic_info['Filename'])
                 if (pic_info['ERROR']== '404'):
@@ -99,27 +105,36 @@ def runsDaily():
                     figure_Fail += 1
             else:
                 try:
-                    latest_length=pic_info['Length']
-                    stored_length=log_buffer[pic_info['Filename']] [-1] ['Length']
-                    if latest_length==stored_length:                                    #dict中的图片并未过时（判断标准：大小不变）,ignore
+                    latest_ETag=pic_info['ETag']
+                    stored_ETag=log_buffer[pic_info['Filename']] [-1] ['ETag']
+                    if latest_ETag==stored_ETag:                                    #dict中的图片并未过时（判断标准：大小不变）,ignore
                         print('ignoring\t\t'+pic_info['Filename'])
                         Figure_ignore += 1
                     else:                                                               #为dict中的图片追加新的时间信息,update
-                        log_buffer[pic_info['Filename']].append({'Lastmod':pic_info['Lastmod'],'Length':pic_info['Length']})
+                        log_buffer[pic_info['Filename']].append({'Save_in':next(save_to),'ETag':pic_info['ETag']})
                         print('Updated\t\t\t'+pic_info['Filename'])
+                        download_queue.append(pic_info['Path'])
                         Figure_update += 1
                 except KeyError:                                                        #添加新图片信息,add
-                    log_buffer[pic_info['Filename']]=([{'Lastmod':pic_info['Lastmod'],'Length':pic_info['Length']}])
+                    log_buffer[pic_info['Filename']]=([{'Save_in':next(save_to),'ETag':pic_info['ETag']}])
                     print('adding new img\t\t'+pic_info['Filename'])
+                    download_queue.append(pic_info['Path'])
                     Figure_added += 1
 
     print('\n404:\t\t',figure_404,'\nFailed:\t\t',figure_Fail,'\nUnchanged:\t',Figure_ignore,\
     '\nAdded:\t\t',Figure_added,'\nUpdated:\t',Figure_update)
-    
+
     with open(data_folder+'/'+'update_history.txt','w') as f:#写回图块更新史文件
         f.seek(0)
         f.write(str(log_buffer))
         f.truncate()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:    #增量？下载图片
+        for pic_info2 in executor.map(dealWithPicurl,download_queue,getImgdir(image_folder,True),whetherDownload(True)): 
+            if 'ERROR' in pic_info2.keys():                                              #抓取图片头失败,pass
+                print(pic_info2['ERROR']+'\t\t\t'+pic_info2['Filename'])
+            else:
+                print('Downloaded'+'\t\t'+pic_info2['Filename'])
 
 '''每周运行，和上周的图片信息比较并下载长度变化了的图块，保存图片到当天的文件夹里，收录`图片历史`大型dict。
 '''
