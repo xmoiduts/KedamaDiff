0f55d98b1cd19b988eb425652648cc6d4165fba5
xmoiduts <xmoiduts@users.noreply.github.com>
2017-12-13 23:53:37 +0800

    Save images that actually changed,
    not only updated by ETag but also its content.
    TO DO : Multithreading efficiency dropped to lower than single thread, to be optimized.

--- crawl.py
+++ crawl.py
@@ -2,6 +2,9 @@ import requests,os
 import time,datetime
 import concurrent.futures
 import ast,json
+import hashlib
+import logging
+
 
 # https://map.nyaacat.com/kedama/v2_daytime/0/3/3/2/3/2/3/1/1.jpg?c=1510454854  
 map_domain='https://map.nyaacat.com/kedama'
@@ -9,8 +12,8 @@ map_name='v2_daytime'
 refcode='c=1510454854'
 image_folder=r'images/'+map_name
 data_folder =r'data/'+map_name
-max_threads=32
-crawl_zones=['/1/2/2/2','/0/3/3/3','/0/3/3/2','/1/2/2/3','/2/1/1/0','/2/1/1/1','/3/0/0/0','/3/0/0/1']
+max_threads=4
+crawl_zones=['/1/2/2/2']
 #'/1/2/2/2','/0/3/3/3','/0/3/3/2','/1/2/2/3','/2/1/1/0','/2/1/1/1','/3/0/0/0','/3/0/0/1'
 crawl_level=8
 
@@ -20,14 +23,12 @@ def teaseImage(URL):
     return r.headers
     #print(r.headers['Last-Modified'],r.headers['Content-Length']) #404-"KeyError"error.
 
-'''下载给定URL的文件并重命名保存到指定目录中。'''
-def downloadImage(URL,path,filename):
+'''下载给定URL的文件并返回。'''
+def downloadImage(URL):
     r=requests.get(URL,stream='True')
     if r.status_code==200:
         img=r.raw.read()
-        with open(path+filename,'wb') as f:
-            f.write(img)
-            f.close()
+        return img
 
 '''输入[多个可采集区域的前缀]和采集深度，生成['pic_name','文件名']。'''
 def makePicName(prefixes,depth):
@@ -45,20 +46,20 @@ def makePicName(prefixes,depth):
 def dealWithPicurl(pic_tuple,save_to,download=False):
     url=map_domain+'/'+map_name+pic_tuple[0]+'?'+refcode
     filename=pic_tuple[1]
-    try:
-        
-        if download==True:
-            downloadImage(url,save_to,filename)
-            return {'Filename':filename}
-        else:
+    if download==True: 
+        img = downloadImage(url)
+        return img,filename #fail的异常丢出去
+    else:
+        try:
             rh=teaseImage(url)
             return({'Path':pic_tuple,'Filename':filename,'Lastmod':rh['Last-Modified'],'ETag':rh['ETag']})
-    except KeyError:
-        return({'Filename':filename,'ERROR':'404'})
-    except requests.exceptions.ConnectionError:
-        return({'Filename':filename,'ERROR':'Fail'})
-    except requests.exceptions.ReadTimeout:
-        return({'Filename':filename,'ERROR':'Fail'})
+        except KeyError:
+            return({'Filename':filename,'ERROR':'404'})
+        except requests.exceptions.ConnectionError:
+            return({'Filename':filename,'ERROR':'Fail'})
+        except requests.exceptions.ReadTimeout:
+            return({'Filename':filename,'ERROR':'Fail'})
+
 
 '''永远返回/image_folder/年月日'''
 '''我也不想写这个的，但是直接传image_folder值,后面的executor.map()就只能执行17次'''
@@ -78,7 +79,7 @@ def whetherDownload(download):
 
 '''每天运行，探测选定区域内的图块信息但不下载图片，收录抓取日志，收录`活跃度`大型dict'''
 def runsDaily():
-    figure_404=figure_Fail=Figure_ignore=Figure_added=Figure_update=0
+    Figure_404=Figure_Fail=Figure_ignore=Figure_added=Figure_update=Figure_changed=0
     download=False      #日更不下载图片，只收录图片头长度发生了变化的图块信息
     log_buffer={}
     download_queue=[]
@@ -101,40 +102,56 @@ def runsDaily():
             if 'ERROR' in pic_info.keys():                                              #抓取图片头失败,pass
                 print(pic_info['ERROR']+'\t\t\t'+pic_info['Filename'])
                 if (pic_info['ERROR']== '404'):
-                    figure_404  += 1
+                    Figure_404  += 1
                 else :
-                    figure_Fail += 1
+                    Figure_Fail += 1
             else:
                 try:
                     latest_ETag=pic_info['ETag']
                     stored_ETag=log_buffer[pic_info['Filename']] [-1] ['ETag']
-                    if latest_ETag==stored_ETag:                                    #dict中的图片并未过时（判断标准：大小不变）,ignore
+                    if latest_ETag==stored_ETag:                                        #dict中的图片并未过时（判断标准：ETag不变）,ignore
                         print('ignoring\t\t'+pic_info['Filename'])
                         Figure_ignore += 1
-                    else:                                                               #为dict中的图片追加新的时间信息,update
-                        log_buffer[pic_info['Filename']].append({'Save_in':next(save_to),'ETag':pic_info['ETag']})#如果同一天跑了两次怎么办
-                        print('Updated\t\t\t'+pic_info['Filename'])
-                        download_queue.append(pic_info['Path'])
+                    else:                                                               #Etag变化，图片可能更新但不保准，在此下载判断。
+                        try:
+                            img,filename = dealWithPicurl(pic_info['Path'],save_to,True)#联网失败怎么办
+                            latest_img = log_buffer[filename][-1]['Save_in']+filename
+                            with open (latest_img,'rb') as fh:
+                                if hashlib.sha1(fh.read()).hexdigest()!=hashlib.sha1(img).hexdigest():   #图片内容更新了，存盘并为dict追加新的时间信息。
+                                    if next(save_to) == log_buffer[filename][-1]['Save_in'] :
+                                        print('Replaced\t\t'+pic_info['Filename'])         #同一天跑的两次之间图片发生了更新，替换同名的较旧文件，修改dict最后一条，Replace，
+                                        del log_buffer[filename][-1]
+                                    else:
+                                        print('(Updated)\t\t'+pic_info['Filename']) #否则(两张图片虽然不同但不在一天)，ignore
+                                        
+                                    log_buffer[filename].append({'Save_in':next(save_to),'ETag':pic_info['ETag']})#并且向dict追加图片信息并保存图片
+                                    with open(next(save_to)+filename,'wb') as f:
+                                        f.write(img)
+                                        f.close()
+                                    Figure_changed += 1
+                                else:                       #图片内容未更新,ignore
+                                    Figure_ignore += 1
+                                    print('FakeChange\t\t'+pic_info['Filename'])
+                        except requests.exceptions.ReadTimeout:
+                            print('Failed'+'\t\t\t'+pic_info['Filename'])
+                            Figure_Fail += 1
                         Figure_update += 1
                 except KeyError:                                                        #添加新图片信息,add
                     log_buffer[pic_info['Filename']]=([{'Save_in':next(save_to),'ETag':pic_info['ETag']}])
                     print('adding new img\t\t'+pic_info['Filename'])
-                    download_queue.append(pic_info['Path'])
+                    img,filename = dealWithPicurl(pic_info['Path'],save_to,True)
+                    with open(next(save_to)+filename,'wb') as f:
+                        f.write(img)
+                        f.close()
                     Figure_added += 1
 
-    print('\n404:\t\t',figure_404,'\nFailed:\t\t',figure_Fail,'\nUnchanged:\t',Figure_ignore,\
-    '\nAdded:\t\t',Figure_added,'\nUpdated:\t',Figure_update)
+    print('\n404:\t\t',Figure_404,'\nFailed:\t\t',Figure_Fail,'\nUnchanged:\t',Figure_ignore,\
+    '\nAdded:\t\t',Figure_added,'\nUpdated:\t',Figure_update,'\nActually Changed:\t',Figure_changed)
 
     with open(data_folder+'/'+'update_history.json','w') as f:#写回 图块更新史文件
         json.dump(log_buffer,f,indent=2)
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:    #增量？下载图片
-        for pic_info2 in executor.map(dealWithPicurl,download_queue,getImgdir(image_folder,True),whetherDownload(True)): 
-            if 'ERROR' in pic_info2.keys():                                              #抓取图片头失败,pass
-                print(pic_info2['ERROR']+'\t\t\t'+pic_info2['Filename'])
-            else:
-                print('Downloaded'+'\t\t'+pic_info2['Filename'])
 
-'''每周运行，和上周的图片信息比较并下载长度变化了的图块，保存图片到当天的文件夹里，收录`图片历史`大型dict。
+'''每周运行，和上周的图片信息比较并下载Etag的图块，保存SHA1变动了的图片到当天的文件夹里，收录`图片历史`大型json。
 '''
 runsDaily()
